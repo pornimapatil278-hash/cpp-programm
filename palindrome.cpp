ca46c77026b2a199626248374db2b55c0aa2a00c
pornimapatil278-hash <pornimapatil278@gmail.com>
2026-07-29 04:56:00 +0000

    ChAnges

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 int main()
 {
-int n;
-int temp;
-int rev=0;
-cout<<"Enter number: ";
-cin>>n;
-
-}
+    int n;
+    int temp;
+    int rev=0;
+    cout<<"Enter number: ";
+    cin>>n;
+    temp=n;
+    while(n!=0)
+    {
+        rev=rev*10+n%10;
+        n/=10;
+    }
+    if(temp==rev)
+      cout<<"Palindrome";
+    else
+      cout<<"Not Palindrome";
+    return 0;
+} 
