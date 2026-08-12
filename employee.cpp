6882b281e586c9e5be25f67bc1222cd52426f9ed
pornimapatil278-hash <pornimapatil278@gmail.com>
2026-08-12 05:29:53 +0000

    change

---
+++ employee.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Employee
+{
+    int employeeId;
+    string employeeName;
+    Float salary;
+    string department;
+
+ public:
+   void input()
+   {
+    cout<<"Enter employeeId:";
+    cin>>""
+   }
+}
