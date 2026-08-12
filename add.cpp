d93f83f06a735895afbada75c357f47c978bd859
pornimapatil278-hash <pornimapatil278@gmail.com>
2026-08-12 08:56:30 +0000

    change

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    cout<<"Addition="<<a+b<<endl;
+    cout<<"Substraction="<<a-b<<endl;
+    cout<<"Multiplication="<<a*b<<endl;
+    cout<<"Division="<<a/b<<endl;
+    cout<<"Modulus="<<a%b<<endl;
+    return 0;
+}
