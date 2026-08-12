797ffdc92367cd5793f0ee5ee2266389803f4054
pornimapatil278-hash <pornimapatil278@gmail.com>
2026-08-12 09:05:38 +0000

    change

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float length;
+    float width;
+    cout<<"Enter length:";
+    cin>>length;
+    cout<<"Enter width:";
+    cin>>width;
+    cout<<"Area="<<length*width<<endl;
+    cout<<"Perimeter="<<2*(length+width);
+    return 0;
+}
